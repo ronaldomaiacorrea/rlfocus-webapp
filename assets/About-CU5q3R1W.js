02dfce792185671bce1496bac14c3a7ad77d5e33
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-11-21 17:44:36 -0800

    Updates

--- assets/About-CU5q3R1W.js
+++ assets/About-CU5q3R1W.js
@@ -1,4 +1,4 @@
-import{c as o,j as e,L as t}from"./index-l4q2c0jn.js";import{m as a}from"./proxy-DOdOeSP3.js";/**
+import{c as o,j as e,L as t}from"./index-BZfjnSGy.js";import{m as a}from"./proxy-BlwE0Zf7.js";/**
  * @license lucide-react v0.454.0 - ISC
  *
  * This source code is licensed under the ISC license.
