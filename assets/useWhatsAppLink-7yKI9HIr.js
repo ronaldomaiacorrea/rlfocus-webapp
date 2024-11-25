ab29b8d4364de320b844d09401af55f95ff32572
RonaldoMaia <ronaldomaiacorrea@outlook.com>
2024-11-24 16:51:48 -0800

    Updates

--- assets/useWhatsAppLink-7yKI9HIr.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-Cb4J_bhB.js";const r=()=>{const s="5512991876712",e="Olá, gostaria de mais informações sobre os seus serviços!";return t.useMemo(()=>{const o=encodeURIComponent(e);return`https://wa.me/${s}?text=${o}`},[s,e])};export{r as u};
---
+++ assets/useWhatsAppLink-C-xllSXW.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-D0sY8Icf.js";const r=()=>{const s="5512991876712",e="Olá, gostaria de mais informações sobre os seus serviços!";return t.useMemo(()=>{const o=encodeURIComponent(e);return`https://wa.me/${s}?text=${o}`},[s,e])};export{r as u};
